37fd5698ba5081a4aeaba78fa95676ff13856109
lior <lioresabag@gmail.com>
2016-06-15 00:02:29 +0300

    Add FindMeApp proj to git

---
+++ FindMeApp/app/src/main/java/com/demo/lior/app/map/FindMe.java
@@ -0,0 +1,257 @@
+package com.demo.lior.app.map;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+/*
+    Lior Esabag
+*/
+
+public class FindMe extends FragmentActivity implements OnMapReadyCallback, LocationListener, android.location.GpsStatus.Listener {
+
+    private GoogleMap mMap;
+    LatLng latLng;
+    List<android.location.Address> addresses = null;
+    Context mContext;
+    double latitude, longitude;
+    private boolean canGetLocation = false;
+    LocationManager mLocationManager;
+    TextView tvAddress;
+    private GpsStatus mStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkGpsStatus(mLocationManager);
+        setContentView(R.layout.activity_maps);
+        mContext = getApplicationContext();
+        tvAddress = (TextView) findViewById(R.id.address);
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLocationManager.addGpsStatusListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+        mMap = mapFragment.getMap();
+        // Getting Google Play availability status
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+
+        } else {
+            // Google Play Services are available
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            // Getting GoogleMap object from the fragment
+
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            Criteria cr = new Criteria();
+            cr.setAccuracy(Criteria.ACCURACY_FINE);
+            cr.setPowerRequirement(Criteria.POWER_LOW);
+            String provider = locationManager.getBestProvider(cr, true);
+
+            Location myLocation = getLastKnownLocation();
+            if (myLocation != null) {
+                latitude = myLocation.getLatitude();
+                longitude = myLocation.getLongitude();
+                latLng = new LatLng(latitude, longitude);
+            }
+            Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+            try {
+                addresses = geoCoder.getFromLocation(latitude, longitude, 1);
+                //tvAddress.setText("Your location: " + addresses.get(0).getAddressLine(1));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void checkGpsStatus(LocationManager mLocationManager) {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        if ( !mLocationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    private Location getLastKnownLocation() {
+        mLocationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = mLocationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return bestLocation;
+            }
+            Location l = mLocationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(mContext, "onRestart" , Toast.LENGTH_SHORT).show();
+        displayAddress();
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        mMap = mMap;
+        latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("I'M HERE")
+                .snippet("U FindMe")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.airplane)));
+               // .snippet(addresses.get(0).getAddressLine(1)));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(latLng)      // Sets the center of the map to Mountain View
+                .zoom(16)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        tvAddress.setText("");
+        tvAddress.setText("Location changed -> Latitude:" + latitude + ", Longitude:" + longitude);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+        mStatus = mLocationManager.getGpsStatus(mStatus);
+        switch (event) {
+            case GpsStatus.GPS_EVENT_STARTED:
+                Toast.makeText(mContext, "GPS_EVENT_STARTED" , Toast.LENGTH_SHORT).show();
+                displayAddress();
+                break;
+
+            case GpsStatus.GPS_EVENT_STOPPED:
+                Toast.makeText(mContext, "GPS_EVENT_STOPPED" , Toast.LENGTH_SHORT).show();
+                break;
+
+            case GpsStatus.GPS_EVENT_FIRST_FIX:
+                Toast.makeText(mContext, "GPS_EVENT_FIRST_FIX" , Toast.LENGTH_SHORT).show();
+                break;
+
+            case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+                break;
+        }
+    }
+
+    private void displayAddress() {
+        Location myLocation = getLastKnownLocation();
+        if (myLocation != null) {
+            latitude = myLocation.getLatitude();
+            longitude = myLocation.getLongitude();
+            latLng = new LatLng(latitude, longitude);
+        }
+        Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            addresses = geoCoder.getFromLocation(latitude, longitude, 1);
+            tvAddress.setText("Your location: " + addresses.get(0).getAddressLine(1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
